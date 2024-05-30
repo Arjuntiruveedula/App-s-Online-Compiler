0daa44c0f0aeaaa878f2db775a4435b4a2c98c4a
Apparao Tiruveedula <131840796+Arjuntiruveedula@users.noreply.github.com>
2024-05-30 21:29:11 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ const app = express();
 
 app.use(cors(
   {
-  origin:["https://app-s-online-compiler-api.vercel.app"],
+  origin:["https://appro-s-online-compiler.vercel.app"],
   methods:["POST","GET"],
   credentials: true
 }));
